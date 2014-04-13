63897f9ea790ce9c3f949e2c39bdcbbd18364ae5
Gerard Cardona <gerard@ThinkPad.ThinkPad>
2014-04-13 22:16:08 +0100

    new article implementation

--- article/views.py
+++ article/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from article.models import Article
 from django.template.loader import get_template
 from django.template import Context
+from django.core.context_processors import csrf
+from django.shortcuts import render_to_response
 
 # Create your views here.
 
 
 def article_all(request):
-    t = get_template('list.html')
     articles = Article.all()
-    html = t.render(Context({'articles': articles}))
+    c = {'articles': articles}
+    c.update(csrf(request))
 
-    return HttpResponse(html)
+    return render_to_response("list.html", c)
 
 
 def article_detail(request, article_id):
@@ -21,3 +24,14 @@ def article_detail(request, article_id):
     html = t.render(Context({'article': article}))
 
     return HttpResponse(html)
+
+
+def article_new(request):
+    article = Article()
+    article.title = request.POST['title']
+    article.body = request.POST['new_content']
+    # To be refactored once users are enabled. (Should be current user)
+    article.author = 'GCardona'
+    article.put()
+
+    return HttpResponseRedirect('/article/all/')
--- djanglog/urls.py
+++ djanglog/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
 
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'article.views.article_all'),
-    url(r'^article/all', 'article.views.article_all'),
+    url(r'^article/all/$', 'article.views.article_all'),
     url(r'^article/(?P<article_id>\d+)/$', 'article.views.article_detail'),
+    url(r'^article/new/$', 'article.views.article_new'),
 )
