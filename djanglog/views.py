d7410455e357b3bf075f922d7faeb5e45fc3b4d5
GCardona <Gerard@gcardona.net>
2014-04-17 00:12:46 +0100

    Take out unnecessary imports

--- djanglog/views.py
+++ djanglog/views.py
@@ -1,10 +1,3 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-from article.models import Article
-from django.template.loader import get_template
-from django.template import Context
-from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
 from google.appengine.api import users
 
